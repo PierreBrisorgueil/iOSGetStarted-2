2cfb42921c0fd8f77cf610e68f66ec702f9a0c0a
Pierre <pierrebrisorgueil@me.com>
2015-10-06 12:17:56 +0200

    Fetch Data from Json

--- StepOne/ViewController.swift
+++ StepOne/ViewController.swift
@@ -2,17 +2,35 @@
 //  ViewController.swift
 //  StepOne
 //
-//  Created by RYPE on 24/04/2015.
-//  Copyright (c) 2015 RYPE. All rights reserved.
+//  Created by WeAreOpenSource.me on 24/04/2015.
+//  Copyright (c) 2015 RYPE. WeAreOpenSource.me rights reserved.
 //
+
+// tuto : http://jamesonquave.com/blog/developing-ios-apps-using-swift-tutorial-part-2/
 
 import UIKit
 
 class ViewController: UIViewController  {
 
+    /*************************************************/
+    // Main
+    /*************************************************/
+
+    // Boulet
+    /**************************************/
+    @IBOutlet weak var appsTableView: UITableView!
+    
+    // Var
+    /**************************************/
+    var tableData = []
+
+    // Base
+    /**************************************/
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        searchItunesFor("La Baule")
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,19 +38,78 @@ class ViewController: UIViewController  {
         // Dispose of any resources that can be recreated.
     }
 
-
+    
+    
+    // Table View
+    /**************************************/
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return tableData.count
     }
+    
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "MyTestCell")
         
-        cell.textLabel?.text = "Row #\(indexPath.row)"
-        cell.detailTextLabel?.text = "Subtitle #\(indexPath.row)"
-        
+        if let rowData: NSDictionary = self.tableData[indexPath.row] as? NSDictionary,
+            // Grab the artworkUrl60 key to get an image URL for the app's thumbnail
+            urlString = rowData["artworkUrl60"] as? String,
+            // Create an NSURL instance from the String URL we get from the API
+            imgURL = NSURL(string: urlString),
+            // Get the formatted price string for display in the subtitle
+            formattedPrice = rowData["formattedPrice"] as? String,
+            // Download an NSData representation of the image at the URL
+            imgData = NSData(contentsOfURL: imgURL),
+            // Get the track name
+            trackName = rowData["trackName"] as? String {
+                // Get the formatted price string for display in the subtitle
+                cell.detailTextLabel?.text = formattedPrice
+                // Update the imageView cell to use the downloaded image data
+                cell.imageView?.image = UIImage(data: imgData)
+                // Update the textLabel text to use the trackName from the API
+                cell.textLabel?.text = trackName
+        }
         return cell
+    }
+    
+    
+    /*************************************************/
+    // Functions
+    /*************************************************/
+    
+    func searchItunesFor(searchTerm: String) {
+        // The iTunes API wants multiple terms separated by + symbols, so replace spaces with + signs
+        let itunesSearchTerm = searchTerm.stringByReplacingOccurrencesOfString(" ", withString: "+", options: NSStringCompareOptions.CaseInsensitiveSearch, range: nil)
+        
+        // Now escape anything else that isn't URL-friendly
+        if let escapedSearchTerm = itunesSearchTerm.stringByAddingPercentEncodingWithAllowedCharacters(.URLHostAllowedCharacterSet()) {
+            let urlPath = "http://itunes.apple.com/search?term=\(escapedSearchTerm)&media=software"
+            let url = NSURL(string: urlPath)
+            let session = NSURLSession.sharedSession()
+            let task = session.dataTaskWithURL(url!, completionHandler: {data, response, error -> Void in
+                print("Task completed")
+                if(error != nil) {
+                    // If there is an error in the web request, print it to the console
+                    print(error!.localizedDescription)
+                }
+                
+                do {
+                    let jsonResult = try NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.MutableContainers) as! NSDictionary
+                    if let results: NSArray = jsonResult["results"] as? NSArray {
+                        dispatch_async(dispatch_get_main_queue(), {
+                            self.tableData = results
+                            self.appsTableView!.reloadData()
+                        })
+                    }
+                } catch let error as NSError {
+                    print(error.description)
+                }
+                
+            })
+            // The task is just an object with all these properties set
+            // In order to actually make the web request, we need to "resume"
+            task.resume()
+        }
     }
 }
 
